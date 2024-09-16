92083e026cae8e925fb24e0cf6746b8dc9db3147
Nikhal Kumar Rana <108257667+Nikhal-Kumar-Rana@users.noreply.github.com>
2024-09-16 05:59:58 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import io
 app = Flask(__name__)
 
 # Load the trained model from the .pkl file
-with open('RandomForest_model.pkl', 'rb') as model_file:
+with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
 # Define the columns expected in the input data
